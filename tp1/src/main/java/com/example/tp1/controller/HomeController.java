fa7bdddfe15e448394d39b9ed43c62d80da2c3f8
nicolas-10 <alawenicolas@gmail.com>
2022-04-23 11:45:33 +0000

    Complément des changements récent

--- tp1/src/main/java/com/example/tp1/controller/HomeController.java
+++ tp1/src/main/java/com/example/tp1/controller/HomeController.java
@@ -5,9 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+    @GetMapping("/logout")
+    public  String logoutPage(){
+        return "logout";
+    }
 
     @GetMapping("/home")
-    public String afficherHello(){
-        return "exemple";
+    public String afficherHome(Model model){
+        model.addAttribute("Total" ,produitService.countQte());
+        model.addAttribute("produitBelow",produitService.produitBelow());
+        return "Index";
     }
 }
